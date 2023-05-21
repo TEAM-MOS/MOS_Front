bd379e4ed9c21be47441993ddd3ac2abeb3fb69b
JIEUNA-NA <sallykim2018@naver.com>
2023-05-21 23:39:26 +0900

    fix(survey): customVIew 오류 수정

--- MOS/SurveyInputView.swift
+++ MOS/SurveyInputView.swift
@@ -11,6 +11,12 @@ import UIKit
 @IBDesignable
 class SurveyInputView: UIView {
     
+    @IBOutlet var questionNumBackground: UIView!
+    
+    @IBOutlet var questionNumLabel: UILabel!
+    
+    @IBOutlet var containerView: UIView!
+    
     @IBOutlet weak var questionLabel: UILabel!
     
     @IBOutlet weak var button5: UIButton!
@@ -29,6 +35,51 @@ class SurveyInputView: UIView {
         didSet {
             DispatchQueue.main.async {
                 self.questionLabel.text = self.title
+            }
+        }
+    }
+    
+    @IBInspectable
+    var containerborder: CGFloat = 0 {
+        didSet {
+            DispatchQueue.main.async {
+                self.containerView.layer.borderWidth = self.containerborder
+                self.containerView.layer.borderColor = UIColor(named: "gray-2")?.cgColor
+                self.containerView.layer.cornerRadius = 10
+            }
+        }
+    }
+    
+    @IBInspectable
+    var buttonborder: CGFloat = 0 {
+        didSet {
+            DispatchQueue.main.async {
+                self.button1.layer.cornerRadius = self.button1.frame.height/2
+                self.button1.layer.borderWidth = self.containerborder
+                self.button1.layer.borderColor = UIColor(named: "gray-2")?.cgColor
+                self.button2.layer.cornerRadius = self.button1.frame.height/2
+                self.button2.layer.borderWidth = self.containerborder
+                self.button2.layer.borderColor = UIColor(named: "gray-2")?.cgColor
+                self.button3.layer.cornerRadius = self.button1.frame.height/2
+                self.button3.layer.borderWidth = self.containerborder
+                self.button3.layer.borderColor = UIColor(named: "gray-2")?.cgColor
+                self.button4.layer.cornerRadius = self.button1.frame.height/2
+                self.button4.layer.borderWidth = self.containerborder
+                self.button4.layer.borderColor = UIColor(named: "gray-2")?.cgColor
+                self.button5.layer.cornerRadius = self.button5.frame.height/2
+                self.button5.layer.borderWidth = self.containerborder
+                self.button5.layer.borderColor = UIColor(named: "gray-2")?.cgColor
+            }
+        }
+    }
+    
+    @IBInspectable
+    var questionNum: String = "" {
+        didSet {
+            DispatchQueue.main.async {
+                self.questionNumBackground.layer.cornerRadius = self.questionNumBackground.frame.height/2
+                self.questionNumLabel.text = self.questionNum
+                
             }
         }
     }
