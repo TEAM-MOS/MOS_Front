670541f4f8f4e632e967b0f7d1e1397ec2b5814d
JIEUNA-NA <sallykim2018@naver.com>
2023-06-07 13:35:49 +0900

    feat: 설무조사에서 홈 화면 이동

--- MOS/SurveyLastVC.swift
+++ MOS/SurveyLastVC.swift
@@ -8,10 +8,18 @@
 import Foundation
 import UIKit
 class SurveyLasttVC: UIViewController {
-   
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.startButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
+
+    @objc func buttonTapped() {
+        print(#fileID, #function, #line, "- StartButton clicked")
+        self.performSegue(withIdentifier: "navToHome", sender: self)
+        }
 
 }
