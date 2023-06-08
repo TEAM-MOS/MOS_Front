ab902fadd0b0ab43436e56f0ebb869e786562c46
yeonsu <drawingbonny@gmail.com>
2023-06-08 23:53:54 +0900

    feat: Add StudyDetailVC

---
+++ MOS/StudyDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  StudyDetailViewController.swift
+//  MOS
+//
+//  Created by yeonsu on 2023/06/08.
+//
+
+import UIKit
+
+class StudyDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
