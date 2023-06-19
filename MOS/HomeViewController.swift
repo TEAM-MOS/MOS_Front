219b333cbe0f6b3abb50cd4afba4248059a1a57b
JIEUNA-NA <sallykim2018@naver.com>
2023-06-19 21:22:38 +0900

    feat(home): scrollview적용 전 중간 커밋

--- MOS/HomeViewController.swift
+++ MOS/HomeViewController.swift
@@ -8,9 +8,17 @@
 import UIKit
 
 class HomeViewController:UIViewController {
+    @IBOutlet weak var moveToTestView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.moveToTestView.layer.cornerRadius = 8
+        self.moveToTestView.layer.shadowColor = UIColor.gray.cgColor //색상
+        self.moveToTestView.layer.shadowOpacity = 0.3 //alpha값
+        self.moveToTestView.layer.shadowRadius = 10 //반경
+        self.moveToTestView.layer.shadowOffset = CGSize(width: 0, height: 0) //위치조정
+        self.moveToTestView.layer.masksToBounds = false
     }
 
 }
