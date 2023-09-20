b69d540f48ca388a947c02b7cdea45da1e27417e
JIEUNA-NA <sallykim2018@naver.com>
2023-09-21 02:35:19 +0900

    feat(home): 스터디 카테고리(산업/기업 분석) 콜렉션 뷰 적용 완료

--- MOS/Controllers/AllViewController.swift
+++ MOS/Controllers/AllViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class AllViewController: UIViewController {
+class AllViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 콜렉션 뷰 생성 및 설정
+        collectionView.dataSource = self
+        collectionView.delegate = self
 
-        // Do any additional setup after loading the view.
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let leadingConstraint = collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20)
+        leadingConstraint.isActive = true
+
+        let trailingConstraint = collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20)
+        trailingConstraint.isActive = true
+
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return StudyList.listAll.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // 재사용 가능한 셀을 가져옴 (indexPath 번째)
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StudyListCell", for: indexPath) as? StudyListCollectionViewCell else {
+            // casting 실패 시
+            return UICollectionViewCell()
+        }
+        
+        let studyListCell = StudyList.list[indexPath.item]
+        // indexPath 안에는 섹션에 대한 정보, 섹션에 들어가는 데이터 정보 등이 있다
+        cell.configure(studyListCell)
+        return cell
+    }
+
     
 
     /*
@@ -26,4 +58,20 @@ class AllViewController: UIViewController {
     }
     */
 
+}
+extension AllViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+           let studyList = StudyList.listAll[indexPath.item]
+           
+           let titleFont = UIFont.systemFont(ofSize: 17) // 적절한 폰트 사용
+           let titleText = studyList.title
+           let titleMaxWidth = collectionView.bounds.width /* 패딩이나 제약조건 조절 */
+           
+           let titleHeight = titleText.height(withConstrainedWidth: titleMaxWidth, font: titleFont)
+           
+           // 제목 레이블 높이를 기반으로 원하는 셀 높이 계산
+        let cellHeight = titleHeight + 130.0 /* 다른 셀 요소에 대한 높이 추가 */
+           
+           return CGSize(width: collectionView.bounds.width, height: cellHeight)
+       }
 }
--- MOS/HomeViewController.swift
+++ MOS/HomeViewController.swift
@@ -12,16 +12,6 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var showMoreStudies: UIImageView!
     @IBOutlet weak var navToSurveyBtn: UIView!
     @IBOutlet weak var studyCollectionView: UICollectionView!
-
-    // StudyList 데이터 배열
-    fileprivate let studyList = [
-        StudyList(category: "산업&기업 분석", title: "2023년 상반기 네이버 IT직군 면접 준비 스터디원 모집합니다 :)", imageName: "ic_home_category1", location: "송파구", date: "2/14(토)", member: "3/4"),
-        StudyList(category: "인/적성", title: "IBK기업은행 공채 준비하는 사람들 모임", imageName: "ic_home_star", location: "동작구", date: "3/12(월)", member: "1/4"),
-        StudyList(category: "어학", title: "오픽은 선택 아닌 필수인 스터디", imageName: "circle", location: "노원구", date: "3/2(월)", member: "3/6"),
-        StudyList(category: "1", title: "3", imageName: "1", location: "송파구", date: "2/14(토)", member: "3/4"),
-        StudyList(category: "1", title: "3", imageName: "1", location: "송파구", date: "2/14(토)", member: "3/4")
-    ]
-
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = true
@@ -37,10 +27,16 @@ class HomeViewController: UIViewController {
         applyShadow()
 
         // 콜렉션 뷰에 대한 설정
-        studyCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         studyCollectionView.dataSource = self
         studyCollectionView.delegate = self
-        studyCollectionView.isScrollEnabled = false
+        
+        studyCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let leadingConstraint = studyCollectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20)
+        leadingConstraint.isActive = true
+
+        let trailingConstraint = studyCollectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20)
+        trailingConstraint.isActive = true
     }
 
     @objc func imageTapped() {
@@ -80,7 +76,7 @@ extension HomeViewController: UICollectionViewDataSource {
             return UICollectionViewCell()
         }
 
-        let studyListItem = studyList[indexPath.row] // indexPath.item 대신 indexPath.row 사용
+        let studyListItem = StudyList.list[indexPath.row] // indexPath.item 대신 indexPath.row 사용
         cell.configure(studyListItem)
         return cell
     }
@@ -88,23 +84,19 @@ extension HomeViewController: UICollectionViewDataSource {
 
 extension HomeViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let studyListItem = studyList[indexPath.item]
-
-        let titleFont = UIFont.systemFont(ofSize: 17) // 적절한 폰트 사용
-        let titleText = studyListItem.title
-        let titleMaxWidth = collectionView.bounds.width - 32 // 패딩이나 제약조건 조절
-
-        let titleHeight = titleText.height(withConstrainedWidth: titleMaxWidth, font: titleFont)
-
-        // 다른 셀 요소에 대한 높이 계산
-        let imageHeight: CGFloat = 120.0 // 이미지 뷰의 높이
-        let locationDateMemberHeight: CGFloat = 30.0 // 위치, 날짜, 멤버 레이블 높이 합
-
-        // 셀의 높이 계산
-        let cellHeight = titleHeight + imageHeight + locationDateMemberHeight + 16.0 /* 다른 간격 및 여백 */
-
-        return CGSize(width: collectionView.bounds.width, height: cellHeight)
-    }
+           let studyList = StudyList.list[indexPath.item]
+           
+           let titleFont = UIFont.systemFont(ofSize: 17) // 적절한 폰트 사용
+           let titleText = studyList.title
+           let titleMaxWidth = collectionView.bounds.width /* 패딩이나 제약조건 조절 */
+           
+           let titleHeight = titleText.height(withConstrainedWidth: titleMaxWidth, font: titleFont)
+           
+           // 제목 레이블 높이를 기반으로 원하는 셀 높이 계산
+        let cellHeight = titleHeight + 130.0 /* 다른 셀 요소에 대한 높이 추가 */
+           
+           return CGSize(width: collectionView.bounds.width, height: cellHeight)
+       }
 }
 
 
