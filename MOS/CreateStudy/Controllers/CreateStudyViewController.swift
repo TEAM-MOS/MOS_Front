c04b7111c3989426967f883aa8c2456bb5e931c0
JIEUNA-NA <sallykim2018@naver.com>
2023-10-04 03:25:40 +0900

    feat(createStudy): 스터디 선택 화면 UI구현 완료

--- MOS/CreateStudy/Controllers/CreateStudyViewController.swift
+++ MOS/CreateStudy/Controllers/CreateStudyViewController.swift
@@ -10,5 +10,6 @@ import UIKit
 class CreateStudyViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
     }
 }
