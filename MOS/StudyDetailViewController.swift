266e067ad4ae5a419bbb76bddd96470f0c6c14b5
yeonsu <drawingbonny@gmail.com>
2023-10-05 19:32:45 +0900

    feat: Add dynamic height on StudyDetailInfoVC

--- MOS/StudyDetailViewController.swift
+++ MOS/StudyDetailViewController.swift
@@ -11,14 +11,15 @@ class StudyDetailViewController: UIViewController {
     
     @IBOutlet var studyDetailView: UIView!
     @IBOutlet weak var containerVC1: UIView!
+    @IBOutlet weak var containerVC2: UIView!
     @IBOutlet weak var heightConst: NSLayoutConstraint!
-    
+    @IBOutlet weak var heightConst2: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // dynamic height 설정
-        setHeightOnStudyOutline()
+        setDynamicHeight()
 
         // 네비게이션 바 수정
         let backButton = UIBarButtonItem()
@@ -41,19 +42,25 @@ class StudyDetailViewController: UIViewController {
         self.present(nextVC, animated: true, completion: nil)
     }
     
-    func setHeightOnStudyOutline() {
-        let con1 = StudyOutlineViewController()
-        // 원하는 높이 값을 계산합니다. 이 예에서는 cellHeight로 가정합니다.
+    func setDynamicHeight() {
         let titleFont = UIFont.systemFont(ofSize: 17) // 적절한 폰트 사용
+        let textFont = UIFont.systemFont(ofSize: 16)
         let titleText =  StudyDetail.list.title
+        let studyText = StudyDetail.list.studyRule
         let titleMaxWidth = containerVC1.bounds.width
+        let studyMaxWidth = containerVC2.bounds.width
 
         let titleHeight = titleText.height(withConstrainedWidth: titleMaxWidth, font: titleFont)
+        let studyHeight = studyText.height(withConstrainedWidth: studyMaxWidth, font: textFont)
+        
         print(titleHeight)
+        print(studyHeight)
         let cellHeight = titleHeight + 120.0 /* 다른 셀 요소에 대한 높이 추가 */
+        let cellHeight2 = studyHeight + 120.0
 
         // NSLayoutConstraint에 새로운 높이 값을 할당합니다.
         heightConst.constant = cellHeight
+        heightConst2.constant = cellHeight2
 
         // Auto Layout 업데이트를 수행하여 제약 조건을 적용합니다.
         view.layoutIfNeeded()
