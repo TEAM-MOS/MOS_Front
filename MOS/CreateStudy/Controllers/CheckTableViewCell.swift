df6c560b7da76fe71d5917ed174c7c2bb2f8bc92
JIEUNA-NA <sallykim2018@naver.com>
2023-10-12 13:49:54 +0900

    중간 임시 커밋

--- MOS/CreateStudy/Controllers/CheckTableViewCell.swift
+++ MOS/CreateStudy/Controllers/CheckTableViewCell.swift
@@ -14,4 +14,12 @@ class CheckTableViewCell: UITableViewCell {
     func set(title: String, checked: Bool) {
         label.text = title
       }
+    
+    override func layoutSubviews() {
+            super.layoutSubviews()
+            
+            // 셀의 컨텐츠 뷰에 여백을 설정
+            let margin: CGFloat = 4.0
+            contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: margin, left: 0, bottom: margin, right: 0))
+        }
 }
