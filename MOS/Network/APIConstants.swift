4039fe811acd68ef351b6f9b907023492b880fb7
yeonsu <drawingbonny@gmail.com>
2023-10-26 02:41:28 +0900

    feat: Add SignUp post api

---
+++ MOS/Network/APIConstants.swift
@@ -0,0 +1,13 @@
+//
+//  APIConstants.swift
+//  MOS
+//
+//  Created by yeonsu on 10/26/23.
+//
+
+import Foundation
+
+struct APIConstants {
+    // MARK: - Base URL
+    static let baseURL = "http://43.200.134.139:8090"
+}
