1ef38c7dab4b8bf7f1bd752dfe13b6ecb38a8468
JIEUN-NA <103167235+JIEUN-NA@users.noreply.github.com>
2023-10-28 04:22:54 +0900

    feat(createStudy): 스택에 쌓인 뷰 제거

--- MOS/CreateStudy/Controllers/FinishVC.swift
+++ MOS/CreateStudy/Controllers/FinishVC.swift
@@ -17,7 +17,12 @@ class FinishVC: UIViewController{
     
     @IBAction func gotoHomeBtnTapped(_ sender: Any) {
         if let tabBarController = self.tabBarController {
-                tabBarController.selectedIndex = 0 // 0은 첫 번째 탭을 나타냅니다. 적절한 인덱스로 변경하세요.
-            }
+               tabBarController.selectedIndex = 0
+           }
+
+           // 현재 뷰 컨트롤러를 스택에서 삭제하고 이전 화면으로 돌아가려면 아래 코드를 사용합니다.
+           if let navigationController = self.navigationController {
+               navigationController.popToRootViewController(animated: true)
+           }
     }
 }
