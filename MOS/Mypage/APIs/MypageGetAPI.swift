8fa0d6602c8379d63f3eb1e35ef44250d258f358
JIEUN-NA <103167235+JIEUN-NA@users.noreply.github.com>
2023-11-13 05:43:05 +0900

    feat(mypage): api 반환 타입 변경

--- MOS/Mypage/APIs/MypageGetAPI.swift
+++ MOS/Mypage/APIs/MypageGetAPI.swift
@@ -12,7 +12,7 @@ class MypageGet {
     static let instance = MypageGet()
     let authToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhY2Nlc3NUb2tlbiIsImV4cCI6MTcwMDE1OTA4MCwiZW1haWwiOiJzYWxseWtpbTIwMThAbmF2ZXIuY29tIn0._bWCMI9Zi_z9hL-tdGrhYELt-XFN3Wu0Z4ck6267OUKEkomdqwDqT-9hnSFDwTd1q9nzE4qlAbaXnv2N7lT7uw"
 
-    func mypageGet(handler: @escaping (_ result: [MypageResultModel]) -> Void) {
+    func mypageGet(handler: @escaping (_ result: MypageResultModel?) -> Void) {
         let url = APIConstants.baseURL + "/profile"
         let headers: HTTPHeaders = [
             .contentType("application/json"),
@@ -22,30 +22,36 @@ class MypageGet {
         AF.request(url, method: .get, headers: headers).response { response in
             switch response.result {
             case .success(let data):
-                print(String(decoding: data!, as: UTF8.self))
                 do {
-                    let jsonResult = try JSONDecoder().decode([MypageResultModel].self, from: data!)
-                    handler(jsonResult)
+                    let jsonResult = try JSONDecoder().decode(MypageResponseModel.self, from: data!)
+                    handler(jsonResult.data)
                 } catch {
                     print("Error decoding JSON:", error)
+                    handler(nil)
                 }
             case .failure(let error):
                 print("Network request failed:", error)
+                handler(nil)
             }
         }
     }
 }
 
+struct MypageResponseModel: Codable {
+    let data: MypageResultModel
+}
+
 struct MypageResultModel: Codable {
     let id: Int
-    let nickname: String
-    let name: String
-    let strDuration: String
-    let endDuration: String
-    let message: String
-    let company: String
-    let tend1: String
-    let tend2: String
+    let nickname: String?
+    let name: String?
+    let strDuration: String?
+    let endDuration: String?
+    let message: String?
+    let company: String?
+    let tend1: String?
+    let tend2: String?
     let roomId: Int?
-    let imageUrl: String
+    let imageUrl: String?
 }
+
--- MOS/Mypage/Controllers/MyPageViewController.swift
+++ MOS/Mypage/Controllers/MyPageViewController.swift
@@ -4,38 +4,38 @@
 //
 //  Created by yeonsu on 2023/05/24.
 //
-
 import UIKit
-
 
 class MyPageViewController: UIViewController {
     @IBOutlet weak var myImage: UIImageView!
     @IBOutlet weak var mypageName: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = true
         getProfile()
     }
-    
+
     func getProfile() {
         MypageGet.instance.mypageGet { [weak self] result in
-            // Check if result is not empty
-            if let mypageResult = result.first {
+            // Check if result is not nil
+            if let mypageResult = result {
                 // Update UI elements with the received data
                 self?.updateUI(with: mypageResult)
             }
         }
     }
-    
+
     func updateUI(with mypageResult: MypageResultModel) {
         mypageName.text = mypageResult.nickname
-        
-        if let imageUrl = URL(string: mypageResult.imageUrl) {
+
+        if let imageUrlString = mypageResult.imageUrl, let imageUrl = URL(string: imageUrlString) {
             myImage.kf.setImage(with: imageUrl)
         } else {
-            // Set a default image if imageUrl is invalid
+            // Set a default image if imageUrl is nil or an invalid URL
+            print("이미지 없음")
             myImage.image = UIImage(named: "defaultImage")
         }
     }
-    
 }
+
