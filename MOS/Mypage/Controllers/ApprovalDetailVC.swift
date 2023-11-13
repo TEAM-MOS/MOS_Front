42c76109e1c135caeb514d601066a595b4f7810a
JIEUN-NA <103167235+JIEUN-NA@users.noreply.github.com>
2023-11-13 23:00:13 +0900

    feat(mypage): 뒤로가기 구현

--- MOS/Mypage/Controllers/ApprovalDetailVC.swift
+++ MOS/Mypage/Controllers/ApprovalDetailVC.swift
@@ -13,4 +13,15 @@ class ApprovalDetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func backBtnTapped(_ sender: UIButton) {
+        guard let navigationControllers = self.navigationController?.viewControllers else { return }
+        for viewController in navigationControllers {
+            print(viewController)
+            if let previousVC = viewController as? MyPageViewController {
+                self.navigationController?.popToViewController(previousVC, animated: false)
+                break
+            }
+        }
+
+    }
 }
