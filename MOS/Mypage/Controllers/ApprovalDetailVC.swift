b7672d87570c1893a0d53fa7504be12cbdb02f9c
JIEUN-NA <103167235+JIEUN-NA@users.noreply.github.com>
2023-11-14 04:31:48 +0900

    design(mypage-approval): 디자인 수정

--- MOS/Mypage/Controllers/ApprovalDetailVC.swift
+++ MOS/Mypage/Controllers/ApprovalDetailVC.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ApprovalDetailVC: UIViewController {
     
+    @IBOutlet weak var studyTitleView: UIView!
+    @IBOutlet weak var approvalView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // studyTitleView의 배경색과 왼쪽 모서리 corner-radius 설정
+       studyTitleView.backgroundColor = UIColor(hex: "F3F3F3")
+       studyTitleView.layer.cornerRadius = 8
+       studyTitleView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
+               
+       // approvalView의 배경색과 오른쪽 모서리 corner-radius 설정
+       approvalView.backgroundColor = UIColor(hex: "D9D9D9")
+       approvalView.layer.cornerRadius = 8
+       approvalView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+    
     }
     @IBAction func backBtnTapped(_ sender: UIButton) {
         guard let navigationControllers = self.navigationController?.viewControllers else { return }
