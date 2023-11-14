858f09ed053d017656b9ed6bc0902fa8d6cdcc75
JIEUN-NA <103167235+JIEUN-NA@users.noreply.github.com>
2023-11-14 17:24:27 +0900

    feat(createStudy): 스터디 생성 후 홈화면 돌아왔을때, 뷰를 다시 그리게 구현

--- MOS/CreateStudy/Controllers/FinishVC.swift
+++ MOS/CreateStudy/Controllers/FinishVC.swift
@@ -16,15 +16,14 @@ class FinishVC: UIViewController{
     
     
     @IBAction func gotoHomeBtnTapped(_ sender: Any) {
-        
-        if let tabBarController = self.tabBarController {
-               tabBarController.selectedIndex = 0
-           }
+            if let tabBarController = self.tabBarController {
+                tabBarController.selectedIndex = 0
+            }
 
-           // 현재 뷰 컨트롤러를 스택에서 삭제하고 이전 화면으로 돌아가려면 아래 코드를 사용합니다.
-           if let navigationController = self.navigationController {
-               navigationController.popToRootViewController(animated: true)
-           }
-    }
+            // 현재 뷰 컨트롤러를 스택에서 삭제하고 이전 화면으로 돌아가려면 아래 코드를 사용합니다.
+            if let navigationController = self.navigationController {
+                navigationController.popToRootViewController(animated:  false)
+            }
+        }
     
 }
