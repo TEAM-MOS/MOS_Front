aa9aebedaf9bb080dd9614c2356ba7a0cddaab01
JIEUN-NA <103167235+JIEUN-NA@users.noreply.github.com>
2023-11-19 01:07:30 +0900

    design(mypage): 내가 개설한 스터디 이미지 및 뷰 corner-radius 설정

--- MOS/Mypage/Controllers/MyStudyVC.swift
+++ MOS/Mypage/Controllers/MyStudyVC.swift
@@ -9,9 +9,31 @@ import Foundation
 import UIKit
 class MystudyVC: UIViewController {
     
+    
+    @IBOutlet weak var bgVIiew: UIView!
+    @IBOutlet weak var profile6: UIImageView!
+    @IBOutlet weak var profile5: UIImageView!
+    @IBOutlet weak var profile4: UIImageView!
+    @IBOutlet weak var profile3: UIImageView!
+    @IBOutlet weak var profile1: UIImageView!
+    @IBOutlet weak var profile2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hidesBottomBarWhenPushed = true
+        
+        bgVIiew.layer.cornerRadius = 8
+        profile1.contentMode = .scaleAspectFill
+        profile1.layer.cornerRadius = profile1.bounds.width / 2
+        profile2.contentMode = .scaleAspectFill
+        profile2.layer.cornerRadius = profile1.bounds.width / 2
+        profile3.contentMode = .scaleAspectFill
+        profile3.layer.cornerRadius = profile1.bounds.width / 2
+        profile4.contentMode = .scaleAspectFill
+        profile4.layer.cornerRadius = profile1.bounds.width / 2
+        profile5.contentMode = .scaleAspectFill
+        profile5.layer.cornerRadius = profile1.bounds.width / 2
+        profile6.contentMode = .scaleAspectFill
+        profile6.layer.cornerRadius = profile1.bounds.width / 2
     }
     
     @IBAction func backBtnTapped(_ sender: UIButton) {
