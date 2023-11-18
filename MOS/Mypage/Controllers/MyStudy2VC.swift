2dc184dce3b6197c46f8b584f9277c75d6f52152
JIEUN-NA <103167235+JIEUN-NA@users.noreply.github.com>
2023-11-19 02:52:51 +0900

    feat(mypage): 스터디원 승인/거부 페이지 UI 구현 및 화면 이동 구현

--- MOS/Mypage/Controllers/MyStudy2VC.swift
+++ MOS/Mypage/Controllers/MyStudy2VC.swift
@@ -1,13 +1,13 @@
 //
-//  MyStudy.swift
+//  MyStudy2VC.swift
 //  MOS
 //
-//  Created by 김지은 on 2023/11/14.
+//  Created by 김지은 on 2023/11/19.
 //
 
 import Foundation
 import UIKit
-class MystudyVC: UIViewController {
+class Mystudy2VC: UIViewController {
     
     var popUp: PopUp!
     
@@ -16,8 +16,8 @@ class MystudyVC: UIViewController {
     @IBOutlet weak var profile5: UIImageView!
     @IBOutlet weak var profile4: UIImageView!
     @IBOutlet weak var profile3: UIImageView!
-    @IBOutlet weak var profile1: UIImageView!
     @IBOutlet weak var profile2: UIImageView!
+    @IBOutlet weak var profile1: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hidesBottomBarWhenPushed = true
@@ -38,15 +38,7 @@ class MystudyVC: UIViewController {
     }
     
     @IBAction func backBtnTapped(_ sender: UIButton) {
-        guard let navigationControllers = self.navigationController?.viewControllers else { return }
-        for viewController in navigationControllers {
-            print(viewController)
-            if let previousVC = viewController as? MyPageViewController {
-                self.navigationController?.popToViewController(previousVC, animated: false)
-                break
-            }
-        }
-        
+        performSegue(withIdentifier: "toMyPage", sender: nil)
     }
     @IBAction func profileBtnTapped(_ sender: Any) {
         self.popUp = PopUp(frame: self.view.frame)
@@ -55,12 +47,6 @@ class MystudyVC: UIViewController {
     }
     
     @objc func nextBtnTapped(){
-        let storyboard = UIStoryboard(name: "MyPage", bundle: nil)
-        if let secondProfileVC = storyboard.instantiateViewController(withIdentifier: "SecondProfile") as? SecondProfile {
-            // 모달로 화면 전환
-            secondProfileVC.modalPresentationStyle = .fullScreen
-            self.present(secondProfileVC, animated: false, completion: nil)
-        }
+        self.popUp.removeFromSuperview()
     }
 }
-
---
+++ MOS/Mypage/Controllers/SecondProfile.swift
@@ -0,0 +1,15 @@
+//
+//  MypageVC.swift
+//  MOS
+//
+//  Created by 김지은 on 2023/11/19.
+//
+
+import Foundation
+import UIKit
+class SecondProfile :UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
