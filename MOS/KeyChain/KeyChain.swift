fd12ac9b89c97bebd9b6d56ed4e56dcc2af0e381
JIEUN-NA <103167235+JIEUN-NA@users.noreply.github.com>
2023-11-22 22:44:57 +0900

    fix(login): 키체인에 토큰 저장 안되는 문제 해결

--- MOS/KeyChain/KeyChain.swift
+++ MOS/KeyChain/KeyChain.swift
@@ -12,76 +12,70 @@ import Security
 
 class KeyChain {
     // Create
-    class func create(account: String, token: String) -> Bool{
-        
+    class func create(account: String, token: String) -> Bool {
         let createQuery: NSDictionary = [
             kSecClass: kSecClassGenericPassword,
-            //            kSecAttrService: server ,
+            // kSecAttrService: server,
             kSecAttrAccount: account,
-            kSecValueData: token.data(using: .utf8, allowLossyConversion: false) as Any
+            kSecValueData: token.data(using: .utf8) as Any
         ]
-        
-        //        SecItemDelete(createQuery)
-        let upadateItem: Bool = {
-            let status : OSStatus = SecItemAdd(createQuery, nil)
-            if status == errSecSuccess{
+
+        let updateItem: Bool = {
+            let status: OSStatus = SecItemAdd(createQuery, nil)
+            if status == errSecSuccess {
                 return true
-            }
-            else if status == errSecDuplicateItem{
+            } else if status == errSecDuplicateItem {
                 return update(account: account, newToken: token)
             }
-            print("addItem Error : \(status.description)")
+            print("addItem Error: \(status.description)")
             return false
         }()
-        
-        return upadateItem
+
+        return updateItem
     }
-    
-    class func update(account: String, newToken: String) -> Bool{
-        let query: [String: Any] = [kSecClass as String:kSecClassGenericPassword,
-                                    kSecAttrAccount as String: account]
-        
-        let attributes: [String: Any] = [kSecValueData as String: newToken]
-        
-        if SecItemUpdate(query as CFDictionary, attributes as CFDictionary) == noErr{
+
+    class func update(account: String, newToken: String) -> Bool {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: account
+        ]
+
+        let attributes: [String: Any] = [
+            kSecValueData as String: newToken.data(using: .utf8) as Any
+        ]
+
+        if SecItemUpdate(query as CFDictionary, attributes as CFDictionary) == noErr {
             print("MosKeychain: Token has changed")
             return true
-        }
-        else{
+        } else {
             // print("Something went wrong trying to update the password")
             return false
         }
     }
-    
+
     // Read
     class func read(account: String) -> String? {
         let query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
-            //            kSecAttrService: server,
+            // kSecAttrService: server,
             kSecAttrAccount as String: account,
             kSecReturnData as String: kCFBooleanTrue as Any,  // CFData 타입으로 불러오라는 의미
-            kSecMatchLimit as String: kSecMatchLimitOne,       // 중복되는 경우, 하나의 값만 불러오라는 의미
-            kSecReturnAttributes as String: true
+            kSecMatchLimit as String: kSecMatchLimitOne       // 중복되는 경우, 하나의 값만 불러오라는 의미
         ]
-        
+
         var dataTypeRef: CFTypeRef?
         var retrieveToken: String = "Something went wrong trying to find the user in the keychain"
-        
-        if SecItemCopyMatching(query as CFDictionary, &dataTypeRef) == noErr {
-            //Extract result
-            if let existingItem = dataTypeRef as? [String: Any],
-               let account = existingItem[kSecAttrAccount as String] as? String,
-               let tokenData = existingItem[kSecValueData as String] as? Data,
-               let token = String(data: tokenData, encoding: .utf8){
-                retrieveToken = token
-                return retrieveToken
-            }
-        }
-        else{
+
+        if SecItemCopyMatching(query as CFDictionary, &dataTypeRef) == noErr,
+           let tokenData = dataTypeRef as? Data,
+           let token = String(data: tokenData, encoding: .utf8) {
+            retrieveToken = token
+        } else {
             print(retrieveToken)
         }
         return retrieveToken
     }
+
     // Delete
     class func delete(key: String) {
         let query: NSDictionary = [
@@ -92,3 +86,4 @@ class KeyChain {
         assert(status == noErr, "failed to delete the value, status code = \(status)")
     }
 }
+
