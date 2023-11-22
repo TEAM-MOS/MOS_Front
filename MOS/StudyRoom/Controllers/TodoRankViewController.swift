950fe2039f940765296cb961740f7f124998400b
yeonsu <drawingbonny@gmail.com>
2023-11-22 11:25:01 +0900

    feat: Add TodoRankViewController

---
+++ MOS/StudyRoom/Controllers/TodoRankViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodoRankViewController.swift
+//  MOS
+//
+//  Created by yeonsu on 11/21/23.
+//
+
+import UIKit
+
+class TodoRankViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
